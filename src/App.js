8563e5be97f71f1573e9a42fa3c5cdcbfb70b3ac
Raine Revere <raineorshine@gmail.com>
2017-09-02 21:13:37 -0600

    Fix balance page. Add navigation.

--- src/App.js
+++ src/App.js
@@ -36,10 +36,17 @@ const App = () => (
 
 const Layout = ({ children}) => (
   <div>
-    <div className='spread center pad-giant'>
+    <div className='spread center'>
       <div className='center-wrapper'>
-        <header>
-          <img src={logo} className='App-logo' alt='logo' />
+
+        <section>
+          <Link to='/' className='button'>Home</Link>
+          <Link to='/challenge' className='button'>Take the Maiden Challenge</Link>
+          <Link to='/balance' className='button'>Check Ether Balance</Link>
+        </section>
+
+        <header className='pad-lg'>
+          <Link to='/'><img src={logo} className='App-logo' alt='logo' /></Link>
         </header>
 
         {children}
@@ -89,10 +96,6 @@ const Home = () =>
     </section>
     <br />
 
-    <section>
-      <Link to='/challenge'>Take the Maiden Challenge</Link>
-    </section>
-
   </Layout>
 
 class Challenge extends Component {
@@ -100,6 +103,7 @@ class Challenge extends Component {
   constructor(props) {
     super(props)
     this.state = {}
+    this.show = this.show.bind(this)
   }
 
   show(step) {
@@ -188,26 +192,65 @@ const Steps = {
 class Balance extends Component {
 
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      address: ''
+      // address: '0x304cc179719bc5b05418d6f7f6783abe45d83090',
     }
+
+    this.changeAddress = this.changeAddress.bind(this)
+
+    // fetch ETH price
+    fetch('https://etherchain.org/api/basic_stats')
+      .then(res => res.json())
+      .then(({ data: { price } }) => {
+        this.setState({ price })
+      })
+      .catch(console.error)
   }
 
   changeAddress(event) {
-    console.log(event.target.value)
-    const that = this
-    web3.eth.getBalance(event.target.value)
-      .then(balance => that.setState({ result: web3.fromWei(balance).toString() }))
-      .catch(console.error)
+    const that = this;
+
+    this.setState({
+      address: event.target.value,
+      result: null,
+      error: false,
+      loading: false
+    })
+
+    if (event.target.value.trim() === '') {
+      return
+    }
+    else if (!web3.utils.isAddress(event.target.value)) {
+      this.setState({ error: true })
+    }
+    else {
+      this.setState({ loading: true })
+      web3.eth.getBalance(event.target.value)
+        .then(balance => {
+          that.setState({
+            loading: false,
+            result: web3.utils.fromWei(balance).toString()
+          })
+        })
+        .catch(console.error)
+    }
+
   }
 
   render() {
     return <Layout>
       <section className='pad'>
+        <h2>Check your Ether balance:</h2>
         <form>
-          <input type='text' placeholder='Enter your Ether address' value={this.state.address} onChange={this.changeAddress}/>
-          <p>{this.state.result}</p>
+          <input className={'address ' + (this.state.error ? 'error' : '')} type='text' placeholder='Enter your Ether address' value={this.state.address} onChange={this.changeAddress}/>
+          {this.state.loading ? <p>loading...</p> :
+           this.state.result ? <div className='text-giant vspace'>
+             <div>{Math.round(this.state.result * 1000) / 1000} ETH</div>
+             <div className='text-dim'>{this.state.price ? '$' + Math.round(+this.state.price.usd * this.state.result * 100) / 100 : null}</div>
+             </div>
+            : null
+          }
         </form>
       </section>
     </Layout>
