42281726438464c3f76de3fca909183901cc21f1
Raine Revere <raineorshine@gmail.com>
2017-12-03 18:05:19 -0700

    ⛵️

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,35 +4,50 @@ import { Link } from 'react-router-dom';
 import logo from '../images/logo.png';
 import logoOnly from '../images/logo-only-small.png';
 
-const Layout = ({ children, activeTab, label = false }) => {
+let boatOffset = 0
 
-  const NavButton = ({ children, to }) =>
-    <Link
-      to={'/' + to}
-      className={activeTab === to ? 'active button' : 'button'}
-    ><span>{children}</span></Link>
+class Layout extends React.Component {
 
-  return <div className='root'>
-    <div className='center'>
+  constructor(props) {
+    super(props)
+    this.state = { boatOffset: 10 }
+  }
 
-      <nav>
-        <NavButton to=''>Home</NavButton>
-        <NavButton to='consulting'>Consulting</NavButton>
-        <NavButton to='about'>About</NavButton>
-      </nav>
+  render() {
 
-      <header>
-        <Link to='/'>
-          <img src={label ? logo : logoOnly} className='App-logo' alt='logo' />
-        </Link>
-      </header>
+    const NavButton = ({ children, to }) =>
+      <Link
+        to={'/' + to}
+        className={this.props.activeTab === to ? 'active button' : 'button'}
+      ><span>{children}</span></Link>
 
-      <section className={activeTab}>
-        {children}
-      </section>
+    return <div className='root'>
+      <div className='center'>
+
+        <nav>
+          <NavButton to=''>Home</NavButton>
+          <NavButton to='consulting'>Consulting</NavButton>
+          <NavButton to='about'>About</NavButton>
+        </nav>
+
+        <header>
+          <Link to='/'>
+            <img src={this.props.label ? logo : logoOnly} className='App-logo' alt='logo' />
+          </Link>
+        </header>
+
+        <section className={this.props.activeTab}>
+          {this.props.children}
+        </section>
+
+      </div>
+
+      <div style={{ marginTop: 300 }}><span style={{ marginLeft: this.state.boatOffset }} className='inline-block rock' onClick={e => {
+        this.setState({ boatOffset: this.state.boatOffset + 20 })
+      }}>⛵</span> ️</div>
 
     </div>
-  </div>
+  }
 }
 
 export default Layout
