9ab9560a1a7003cf59eb7d41e827ba244bc68ab3
Raine Revere <raineorshine@gmail.com>
2018-10-02 07:12:48 -0600

    typo

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () =>
     <h1 className='title'>Maiden Labs conducts comprehensive user learning research and develops authoritative, cross-cultural, and product-agnostic blockchain education to accelerate public trust and adoption.
     </h1>
 
-    <h2 className='vspace'>Maiden Lab is made possible by leading blockchain companies who are invested in the decentralized future. Membership is currently open.</h2>
+    <h2 className='vspace'>Maiden Labs is made possible by leading blockchain companies who are invested in the decentralized future. Membership is currently open.</h2>
 
     <div className="vspace-lg">
       <p>Sign up for our mailing list:</p>
